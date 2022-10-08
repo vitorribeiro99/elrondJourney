fb5e18206177be6fb66fff8677a4781ca6e1ae64
vitorribeiro99 <v99ribeiro@gmail.com>
2022-10-08 18:00:51 +0100

    1st commit

---
+++ mycrowdfunding/wasm/src/lib.rs
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    mycrowdfunding
+    (
+        claim
+        fund
+        getCrowdfundingTokenIdentifier
+        getCurrentFunds
+        getDeadline
+        getDeposit
+        getTarget
+        status
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
