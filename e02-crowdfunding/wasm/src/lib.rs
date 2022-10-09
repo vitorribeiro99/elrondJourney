bb4c0609c13769b2d5e0693d42f2106f8855bf30
vitorribeiro99 <v99ribeiro@gmail.com>
2022-10-09 11:48:14 +0100

    created the smart contract

---
+++ e02-crowdfunding/wasm/src/lib.rs
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    crowdfunding_esdt
+    (
+        claim
+        fund
+        getCrowdfundingTokenIdentifier
+        getCurrentFunds
+        getDeadline
+        getDeposit
+        getTarget
+        status
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
